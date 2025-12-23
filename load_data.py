30820c387f17a1741ccc441245f8ddf009da5ac0
Micah <micahwells2000@gmail.com>
2025-12-23 15:01:47 -0500

    Delete load_data.py

--- load_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data(file_path='housing_data.csv'):
-    try:
-        df = pd.read_csv(file_path)
-        return df
-    except FileNotFoundError:
-        print(f'File not found: {file_path}')
-        return None
